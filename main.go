a164ff52efdeae37bb670b2e50a03ebde50de6bd
Yoshihiro Tanaka <contact@cordea.jp>
2021-05-03 15:23:07 +0900

    divide file

--- main.go
+++ main.go
@@ -2,37 +2,10 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/google"
-	"google.golang.org/api/youtube/v3"
+	"github.com/CORDEA/youtube-migration-tool/youtube"
 	"io/ioutil"
 	"log"
-	"net/http"
 )
-
-func getClient(ctx context.Context, secret []byte, scope ...string) (*http.Client, error) {
-	config, err := google.ConfigFromJSON(secret, scope...)
-	if err != nil {
-		return nil, err
-	}
-	token, err := requestToken(ctx, config)
-	if err != nil {
-		return nil, err
-	}
-	return config.Client(ctx, token), nil
-}
-
-func requestToken(ctx context.Context, config *oauth2.Config) (*oauth2.Token, error) {
-	// TODO
-	url := config.AuthCodeURL("", oauth2.AccessTypeOffline)
-	fmt.Println(url)
-	var code string
-	if _, err := fmt.Scan(&code); err != nil {
-		return nil, err
-	}
-	return config.Exchange(ctx, code)
-}
 
 func main() {
 	ctx := context.Background()
@@ -40,8 +13,7 @@ func main() {
 	if err != nil {
 		log.Fatalln()
 	}
-
-	_, err = getClient(ctx, secret, youtube.YoutubeReadonlyScope)
+	_, err = youtube.ProvideService(ctx, secret)
 	if err != nil {
 		log.Fatalln()
 	}
---
+++ youtube/service_provider.go
@@ -0,0 +1,46 @@
+package youtube
+
+import (
+	"context"
+	"fmt"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/option"
+	"google.golang.org/api/youtube/v3"
+	"net/http"
+)
+
+func ProvideService(ctx context.Context, secret []byte) (*youtube.Service, error) {
+	client, err := getClient(ctx, secret, youtube.YoutubeReadonlyScope)
+	if err != nil {
+		return nil, err
+	}
+	service, err := youtube.NewService(ctx, option.WithHTTPClient(client))
+	if err != nil {
+		return nil, err
+	}
+	return service, nil
+}
+
+func getClient(ctx context.Context, secret []byte, scope ...string) (*http.Client, error) {
+	config, err := google.ConfigFromJSON(secret, scope...)
+	if err != nil {
+		return nil, err
+	}
+	token, err := requestToken(ctx, config)
+	if err != nil {
+		return nil, err
+	}
+	return config.Client(ctx, token), nil
+}
+
+func requestToken(ctx context.Context, config *oauth2.Config) (*oauth2.Token, error) {
+	// TODO
+	url := config.AuthCodeURL("", oauth2.AccessTypeOffline)
+	fmt.Println(url)
+	var code string
+	if _, err := fmt.Scan(&code); err != nil {
+		return nil, err
+	}
+	return config.Exchange(ctx, code)
+}
