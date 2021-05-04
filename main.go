099b02683326f218d17fd292e09f679b0e08c1f5
Yoshihiro Tanaka <contact@cordea.jp>
2021-05-04 15:15:46 +0900

    restore the cache

--- main.go
+++ main.go
@@ -32,28 +32,27 @@ type Playlist struct {
 	Items    []*youtube.PlaylistItem `json:"items"`
 }
 
-func (m *Migrator) fetchMigrationData() *Data {
+func (m *Migrator) fetchMigrationData() (*Data, error) {
 	subs, err := m.subscriptionRepo.FindAll(client.Reading)
 	if err != nil {
-		log.Println(err)
+		return nil, err
 	}
 
 	lists, err := m.playlistRepo.FindAll(client.Reading)
 	if err != nil {
-		log.Println(err)
+		return nil, err
 	}
 
 	var playlists []*Playlist
 	for _, l := range lists {
 		items, err := m.playlistItemRepo.Find(client.Reading, l.Id)
 		if err != nil {
-			log.Println(err)
+			return nil, err
 		}
 		playlists = append(playlists, &Playlist{Playlist: l, Items: items})
-		break
 	}
 
-	return &Data{Subscriptions: subs, Playlists: playlists}
+	return &Data{Subscriptions: subs, Playlists: playlists}, nil
 }
 
 func (m *Migrator) cacheMigrationData(data *Data) error {
@@ -67,8 +66,26 @@ func (m *Migrator) cacheMigrationData(data *Data) error {
 	return err
 }
 
+func (m *Migrator) restoreMigrationData() (*Data, error) {
+	path := filepath.Join(cacheDir, "data.json")
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	data := &Data{}
+	err = json.NewDecoder(f).Decode(data)
+	defer f.Close()
+	return data, err
+}
+
 func (m *Migrator) migrate() {
-	data := m.fetchMigrationData()
+	data, err := m.restoreMigrationData()
+	if err != nil {
+		data, err = m.fetchMigrationData()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
 
 	if err := m.cacheMigrationData(data); err != nil {
 		log.Fatalln(err)
@@ -111,5 +128,5 @@ func main() {
 		playlistItemRepo: playlistItemRepo,
 	}
 
-	_ = migrator.fetchMigrationData()
+	migrator.migrate()
 }
